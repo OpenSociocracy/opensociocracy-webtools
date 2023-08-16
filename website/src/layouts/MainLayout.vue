01aaa88773100abdc03ba18c7896ced7ee0eae00
Brian Winkers <bwinkers@gmail.com>
2023-08-15 17:12:48 -0700

    Dynamic footer for attribution enabled

--- website/src/layouts/MainLayout.vue
+++ website/src/layouts/MainLayout.vue
@@ -1,6 +1,5 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title class="text-h5">
@@ -10,11 +9,46 @@
           OpenSociocracy
         </q-toolbar-title>
         <q-space />
-        <q-btn size="md" dense icon="mdi-github" flat href="https://github.com/OpenSociocracy/opensociocracy-webtools" class="q-px-sm"></q-btn>
-        <q-btn size="md" dense icon="mdi-twitter" flat href="https://twitter.com/OpenSociocracy" class="q-px-sm"></q-btn>
-        <q-btn size="md" dense icon="mdi-facebook" flat href="https://www.facebook.com/OpenSociocracy/" class="q-px-sm"></q-btn>
-        <q-btn size="md" dense icon="mdi-mastodon" flat href="https://fosstodon.org/@opensociocracy" class="q-px-sm"></q-btn>
-        <q-btn size="md" dense icon="mdi-linkedin" flat href="https://www.linkedin.com/company/ultri/" class="q-px-sm"></q-btn>
+        <q-btn
+          size="md"
+          dense
+          icon="mdi-github"
+          flat
+          href="https://github.com/OpenSociocracy/opensociocracy-webtools"
+          class="q-px-sm"
+        ></q-btn>
+        <q-btn
+          size="md"
+          dense
+          icon="mdi-twitter"
+          flat
+          href="https://twitter.com/OpenSociocracy"
+          class="q-px-sm"
+        ></q-btn>
+        <q-btn
+          size="md"
+          dense
+          icon="mdi-facebook"
+          flat
+          href="https://www.facebook.com/OpenSociocracy/"
+          class="q-px-sm"
+        ></q-btn>
+        <q-btn
+          size="md"
+          dense
+          icon="mdi-mastodon"
+          flat
+          href="https://fosstodon.org/@opensociocracy"
+          class="q-px-sm"
+        ></q-btn>
+        <q-btn
+          size="md"
+          dense
+          icon="mdi-linkedin"
+          flat
+          href="https://www.linkedin.com/company/ultri/"
+          class="q-px-sm"
+        ></q-btn>
       </q-toolbar>
 
       <q-tabs align="left">
@@ -22,45 +56,36 @@
         <q-route-tab to="/circles" label="Circles" />
         <q-route-tab to="/canvas" label="Canvas" />
       </q-tabs>
-
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-
-    <q-footer elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <div class="text-center justify-center">Built with <q-icon name="mdi-heart" color="red"></q-icon> by <a href="https://www.ultri.com" class="navbar-text-link">Ultri</a></div>
-        </q-toolbar-title>
-
-      </q-toolbar>
-    </q-footer>
-
+    <u-dynamic-footer />
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
+import UDynamicFooter from "../components/footer/UDynamicFooter.vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
-
+    UDynamicFooter
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
